53a23ece8838e7a235467dc42b31eba918b1c815
Janardhan Bollineni <janardhan.bollineni@colorado.edu>
2023-06-24 04:27:22 +0000

    updates

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,13 @@
 #include "systemcalls.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -17,7 +26,14 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    int ret = system(cmd);
+
+    if (ret == 0) {
+        return true;
+    } else {
+        return false;
+    }
+ 
 }
 
 /**
@@ -36,10 +52,14 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+
+    //printf("\n\nInside do_exec\n");
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
     int i;
+    
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
@@ -47,7 +67,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 /*
  * TODO:
@@ -58,11 +78,52 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
 
+    //printf("Inside do_exec: command is %s & argu is %s\n", command[0], command[2]);
+    fflush(stdout);
+    pid_t pid = fork();
+    if ( pid == 0 ) {
+
+        int ret = execv(command[0], command);
+        perror("execv error: ");
+
+        if (ret == -1) {
+            return false;
+        }
+
+	}
+    if ( pid > 0 ) {
+
+        wait(&status);
+        //printf("End of process %d: \n", pid);
+
+        //printf("wait exit status is %d\n",WIFEXITED(status));
+
+        if (WEXITSTATUS(status) == 0) {
+            //printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+            return true;
+        } 
+        if (WEXITSTATUS(status) == 1) {
+            //printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+            return false;
+        }
+        if (WIFSIGNALED(status)) {
+            //printf("The process ended with kill -%d.\n", WTERMSIG(status));
+        }
+    }
+
+    if (pid < 0) {
+        perror("In fork():");
+        return false;
+
+    }
     va_end(args);
 
     return true;
 }
+
+
 
 /**
 * @param outputfile - The full path to the file to write with command output.
@@ -71,6 +132,9 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+
+    //printf("\n\nInside do_exec_redirect\n");
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -82,7 +146,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 
 /*
@@ -93,7 +157,60 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *
 */
 
+    int status;
+
+    //printf("command is %s & argu is %s\n", command[0], command);
+    fflush(stdout);
+    pid_t pid = fork();
+    if ( pid == 0 ) {
+		//printf( "After the fork, the process identifier (pid) "
+		//        "of the child is %d\n", (int)getpid() );
+
+        int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+        if (fd < 0) { 
+            perror("open"); 
+            abort(); 
+        }
+
+        if (dup2(fd, 1) < 0) { 
+            perror("dup2"); 
+            abort(); 
+        }
+        int ret = execv(command[0], command);
+        perror("execv error: ");
+
+        close(fd);
+
+        if (ret == -1) {
+            return false;
+        }
+	}
+
+    if ( pid > 0 ) {
+		wait(&status);
+        //printf("End of process %d: \n", pid);
+
+        //printf("wait exit status is %d\n",WIFEXITED(status));
+
+        if (WEXITSTATUS(status) == 0) {
+            //printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+            return true;
+        } 
+        if (WEXITSTATUS(status) == 1) {
+            //printf("The process ended with exit(%d).\n", WEXITSTATUS(status));
+            return false;
+        }
+        if (WIFSIGNALED(status)) {
+            //printf("The process ended with kill -%d.\n", WTERMSIG(status));
+        }
+    }
+
+    if (pid < 0) {
+                perror("In fork():");
+        }
+
     va_end(args);
 
     return true;
+
 }
