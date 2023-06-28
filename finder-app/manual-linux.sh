910b271bc596d019deb4301fbc85d21949231145
Janardhan Bollineni <janardhan.bollineni@colorado.edu>
2023-06-28 05:56:24 +0000

    update-manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -39,7 +39,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    #make  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     make  ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
@@ -69,33 +69,37 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}                                                
-    # TODO:  Configure busybox                                                                                                                                                                                               
+    # TODO:  Configure busybox  
+    make distclean
+    make defconfig                                                                                                                                                                                             
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-    make distclean
-    make defconfig
+    
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
     make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a "${OUTDIR}/rootfs/bin/busybox" | grep "program interpreter"
+${CROSS_COMPILE}readelf -a "${OUTDIR}/rootfs/bin/busybox" | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+
+echo "Copying dependency files"
 cp /home/vagrant/_code/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 lib/ld-linux-aarch64.so.1
 cp /home/vagrant/_code/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 lib64/libm.so.6
 cp /home/vagrant/_code/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 lib64/libresolv.so.2
 cp /home/vagrant/_code/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 lib64/libc.so.6
+echo "Done Copying dependency files"
 
 # TODO: Make device nodes
 
-# sudo mknod -m 666 dev/null c 1 3
-# sudo mknod -m 666 dev/console c 5 1
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
 
